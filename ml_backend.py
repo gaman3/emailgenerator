1c7e8cda6ef439eae457c21129a805b0538a3a70
gaman3 <166583726+gaman3@users.noreply.github.com>
2024-04-14 23:42:31 +0530

    Update ml_backend.py

--- ml_backend.py
+++ ml_backend.py
@@ -2,7 +2,7 @@ import openai
 import os
 
 class ml_backend:
-    openai.api_key = os.environ.get('OPENAI_API_KEY')
+    openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
     def generate_email(self, userPrompt ="Write me a professionally sounding email", start="Dear"):
